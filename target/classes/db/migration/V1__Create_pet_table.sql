993099142881c92726f83900063ad6c44faf2362
Tim Eapen <tim.eapen@gmail.com>
2016-03-24 23:14:27 -0400

    deleting commited ignored files

--- target/classes/db/migration/V1__Create_pet_table.sql
+++
@@ -1,5 +0,0 @@
-create table PET (
-    ID bigint auto_increment not null,
-    NAME varchar(100) not null,
-    DESCRIPTION varchar(500) not null
-);
--- target/classes/db/migration/V2__InsertPets.sql
+++
@@ -1,2 +0,0 @@
-insert into pet (ID, NAME, DESCRIPTION) values (1, 'Oliver', 'Domestic Short Hair');
-insert into pet (ID, NAME, DESCRIPTION) values (2, 'Santo Baby', 'Long Haired Beauty');
